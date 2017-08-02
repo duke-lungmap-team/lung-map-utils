daf8afe1a85cf3e7fdc394ef425bc13042b906b7
benneely <nigelneely@gmail.com>
2017-08-02 16:51:42 -0400

    adding a setup.py file for easy import into ihc-image-analysis and also updating a sklearn import due to api changes since that code was written

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lung_map_utils',
+    version='0.1dev',
+    packages=['lung_map_utils',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    author='Duke Lungmap Team',
+    description=('A machine learning pipeline used to predict anatomy from Lungmap images.'),
+    install_requires=['numpy', 'cv2', 'matplotlib', 'scipy', 'pandas', 're', 'scikit-learn'],
+)
