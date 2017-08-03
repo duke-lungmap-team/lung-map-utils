5d443cdf9d2d7dab594b75e1b6e7ce2d442781a1
benneely <nigelneely@gmail.com>
2017-08-03 10:40:08 -0400

    fixing long_description in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1dev',
     packages=['lung_map_utils',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     author='Duke Lungmap Team',
     description=('A machine learning pipeline used to predict anatomy from Lungmap images.'),
     install_requires=['numpy', 'cv2', 'matplotlib', 'scipy', 'pandas', 're', 'scikit-learn'],
