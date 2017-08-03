b251dc507cd91d2688adf7ac2c962c5abb6df4f1
benneely <nigelneely@gmail.com>
2017-08-03 10:48:32 -0400

    removing 're' as a required package, part of core

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     long_description=open('README.md').read(),
     author='Duke Lungmap Team',
     description=('A machine learning pipeline used to predict anatomy from Lungmap images.'),
-    install_requires=['numpy', 'opencv-python', 'matplotlib', 'scipy', 'pandas', 're', 'scikit-learn'],
+    install_requires=['numpy', 'opencv-python', 'matplotlib', 'scipy', 'pandas', 'scikit-learn'],
 )
