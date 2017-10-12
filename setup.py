c137b08424c119def16d698bd7bd68b3254a300d
Scott White <whitews@gmail.com>
2017-10-11 20:05:02 -0400

    massive overhaul, removing unused functionality & bumping to version 1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    name='lung_map_utils',
-    version='0.1dev',
-    packages=['lung_map_utils', ],
+    name='LungMapUtils',
+    version='1.0',
+    packages=['lung_map_utils'],
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
     author='Duke Lungmap Team',
     description='A machine learning pipeline used to predict anatomy from Lungmap images.',
-    install_requires=[
-        'numpy==1.13.1',
-        'opencv-python==3.2.0.7',
-        'matplotlib==2.0.2',
-        'scipy==0.19.1',
-        'pandas==0.19.2',
-        'scikit-learn==0.18.2'],
+    requires=[
+        'numpy (==1.13)',
+        'opencv_python (==3.2.0)',
+        'scipy (==0.19.1)',
+        'pandas (==0.19.2)',
+        'scikit_learn (==0.18.2)'
+    ]
 )
