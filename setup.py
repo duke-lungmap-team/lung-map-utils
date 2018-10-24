5877b598ac3cb83ff89bd6e84d2460d9ba230bc0
whitews <whitews@gmail.com>
2018-10-24 17:20:35 -0400

    complete renaming for project

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='LungMapUtils',
+    name='cv_color_features',
     version='1.2b',
-    packages=['lung_map_utils'],
+    packages=['cv_color_features'],
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
-    author='Duke Lungmap Team',
-    description='A machine learning pipeline used to predict anatomy from Lungmap images.',
+    author='Scott White',
+    description='Utility library to generate feature metrics from regions in color images.',
     install_requires=[
-        'numpy (==1.13)',
-        'opencv-python (==3.2.0.7)',
-        'scipy (==0.19.1)',
-        'pandas (==0.19.2)',
+        'numpy (>=1.13)',
+        'opencv-python (>=3.2.0.7)',
+        'scipy (>=0.19.1)',
+        'pandas (>=0.19.2)',
         'scikit_learn (==0.18.2)'
     ]
 )
